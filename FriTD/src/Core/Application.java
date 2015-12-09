03dc32be6106f75a985b325ef665739e9f1e344b
tomas.kuric <tomas.kuric@doxxbet.com>
2015-12-09 12:50:50 +0100

    hierarchy

---
+++ FriTD/src/Core/Application.java
@@ -0,0 +1,33 @@
+package Core;
+
+import java.util.LinkedList;
+
+import UI.IDisplayableObject;
+
+public class Application {
+
+	
+	public LinkedList<IDisplayableObject> getEnemies(){
+		return null;
+	};
+	public LinkedList<IDisplayableObject> getProjectiles(){
+		return null;
+	};
+	public IDisplayableObject[][] getMap(){
+		return null;
+	};
+	public void start(){
+		
+	};
+	public int getMoney(){
+		return 0;
+	};
+	public int getHP(){
+		return 0;
+	};
+	public void buildTower(String position, String type){
+		
+	};
+	
+	
+}
---
+++ FriTD/src/UI/IDisplayableObject.java
@@ -0,0 +1,9 @@
+package UI;
+
+import java.awt.Graphics2D;
+
+public interface IDisplayableObject {
+
+	public void display(Graphics2D g2d);
+	
+}
