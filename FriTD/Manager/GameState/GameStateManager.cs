4ee2d4770511eb6e77470137586a536c196c5d21
Matúš Mrázik <matus.mrazik2@gmail.com>
2016-02-03 12:52:46 +0100

    GameStateManager added

---
+++ FriTD/Manager/GameState/GameStateManager.cs
@@ -0,0 +1,29 @@
+﻿using Manager.AI;
+using TD.Core;
+
+namespace Manager.GameState
+{
+    class GameStateManager
+    {
+
+        private AICore core;
+
+        public GameStateImage PreviousStateImage { get; private set; }
+
+        public GameStateManager(AICore core)
+        {
+            this.core = core;
+        }
+
+        public string ExecuteDecision(GameStateImage image)
+        {
+
+            return "";
+        }
+
+        public void ExecuteReward(GameStateImage image)
+        {
+
+        }
+    }
+}
