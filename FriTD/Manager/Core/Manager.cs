609fe574f9f3aa9181a7161866335c4b54417e46
tomas.kuric <tomas.kuric@doxxbet.com>
2016-02-09 14:03:36 +0100

    new ctor added, game lvls changed

--- FriTD/Manager/Core/Manager.cs
+++ FriTD/Manager/Core/Manager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.IO;
 using System.Net.Configuration;
 using Manager.AI;
 using Manager.Core.Delayers;
@@ -28,6 +29,12 @@ namespace Manager.Core
             _delayer = new LearningDelayer();
 
            
+        }
+
+        internal void InsertAi(StreamReader reader)
+        {
+            _ai = new AICore(0.09, 1, 0.5,reader);
+            _aiAdapter = new GameStateManager(_ai);
         }
 
         public bool IsAiMode()
