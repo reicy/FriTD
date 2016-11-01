2d1ff07648ec54db83b97d58c79bec6a7f096f06
Pyro2266 <pyro2266@gmail.com>
2016-11-01 15:56:53 +0100

    Workarounds for Unity

--- FriTD/Manager/Core/ManagerBuilder.cs
+++ FriTD/Manager/Core/ManagerBuilder.cs
@@ -1,4 +1,8 @@
-﻿using System.IO;
+﻿using Manager.Kohonen;
+using Manager.MTCore;
+using Manager.QLearning;
+using System.Collections.Generic;
+using System.IO;
 using System.Runtime.Remoting.Messaging;
 
 namespace Manager.Core
@@ -28,6 +32,55 @@ namespace Manager.Core
         public static Manager BuildSimplePlayerManager()
         {
             return new Manager();
+        }
+
+        public static MTCore.MtSingleDaemon BuildMTSingleDaemon(int level, string mobs = null)
+        {
+            string map = null;
+            string levels = null;
+
+            switch(level)
+            {
+                case 0:
+                    map = Properties.Resources.Map;
+                    levels = Properties.Resources.Levels;
+                    break;
+                case 1:
+                    map = Properties.Resources.Map1;
+                    levels = Properties.Resources.Levels1;
+                    break;
+                case 2:
+                    map = Properties.Resources.Map2;
+                    levels = Properties.Resources.Levels2;
+                    break;
+                case 3:
+                    map = Properties.Resources.Map3;
+                    levels = Properties.Resources.Levels3;
+                    break;
+                case 4:
+                    map = Properties.Resources.Map4;
+                    levels = Properties.Resources.Levels4;
+                    break;
+                case 5:
+                    map = Properties.Resources.Map5;
+                    levels = Properties.Resources.Levels5;
+                    break;
+            }
+
+            if(mobs!=null)
+            {
+                levels = mobs;
+            }
+
+
+            var qLearning = new QLearning<KohonenAiState>(0.3, 1, 0.5);
+            var kohonen = new KohonenCore<StateVector>(30, 30, 2, 0.5, 1, 1, 0.5, false);
+            List<KohonenUpdate> updatesQueue = new List<KohonenUpdate>();
+
+            return new MTCore.MtSingleDaemon(kohonen, qLearning, updatesQueue, map, levels, 0, true, false)
+            {
+                IterationStartLearning = 5
+            };
         }
     }
 }
