7f88bb2c5c1f4030e1c8daa5e753d0bfbad137c4
Pyro2266 <pyro2266@gmail.com>
2017-01-06 14:42:34 +0100

    Added new experiment

--- FriTD/TDExperimentLib/Experiments/Experiment05.cs
+++ FriTD/TDExperimentLib/Experiments/Experiment05.cs
@@ -12,7 +12,7 @@ namespace TDExperimentLib.Experiments
     {
         private MtManager manager = new MtManager();
         private Settings s = new Settings();
-        private string cvsFilename = "results.csv";
+        private string csvFilename = "results.csv";
 
         public Experiment05() : base("Exp")
         {
@@ -26,47 +26,107 @@ namespace TDExperimentLib.Experiments
         public override void Run()
         {
             CustomLogger.logToFile = true;
+            string prefix;
+            string postfix;
+            bool withKohonen;
 
-            var prefix = "test1_";
-            RunMap("Map", prefix: prefix);
-            RunMap("Map1", prefix: prefix);
+            postfix = "withKohonen";
+            prefix = "run1_";
+            withKohonen = true;
+            RunMap("Map", prefix: prefix, postfix: postfix, withKohonen: withKohonen);
+            RunMap("Map1", prefix: prefix, postfix: postfix, withKohonen: withKohonen);
+            RunMap("Map2", prefix: prefix, postfix: postfix, withKohonen: withKohonen);
+            RunMap("Map3", prefix: prefix, postfix: postfix, withKohonen: withKohonen);
+            RunMap("Map4", prefix: prefix, postfix: postfix, withKohonen: withKohonen);
+            RunMap("Map5", prefix: prefix, postfix: postfix, withKohonen: withKohonen);
+            postfix = "withoutKohonen";
+            withKohonen = false;
+            RunMap("Map", prefix: prefix, postfix: postfix, withKohonen: withKohonen);
+            RunMap("Map1", prefix: prefix, postfix: postfix, withKohonen: withKohonen);
+            RunMap("Map2", prefix: prefix, postfix: postfix, withKohonen: withKohonen);
+            RunMap("Map3", prefix: prefix, postfix: postfix, withKohonen: withKohonen);
+            RunMap("Map4", prefix: prefix, postfix: postfix, withKohonen: withKohonen);
+            RunMap("Map5", prefix: prefix, postfix: postfix, withKohonen: withKohonen);
 
-            prefix = "test2_";
-            RunMap("Map3", prefix: prefix);
-            RunMap("Map4", prefix: prefix);
+            postfix = "withKohonen";
+            prefix = "run2_";
+            withKohonen = true;
+            RunMap("Map", prefix: prefix, postfix: postfix, withKohonen: withKohonen);
+            RunMap("Map1", prefix: prefix, postfix: postfix, withKohonen: withKohonen);
+            RunMap("Map2", prefix: prefix, postfix: postfix, withKohonen: withKohonen);
+            RunMap("Map3", prefix: prefix, postfix: postfix, withKohonen: withKohonen);
+            RunMap("Map4", prefix: prefix, postfix: postfix, withKohonen: withKohonen);
+            RunMap("Map5", prefix: prefix, postfix: postfix, withKohonen: withKohonen);
+            postfix = "withoutKohonen";
+            withKohonen = false;
+            RunMap("Map", prefix: prefix, postfix: postfix, withKohonen: withKohonen);
+            RunMap("Map1", prefix: prefix, postfix: postfix, withKohonen: withKohonen);
+            RunMap("Map2", prefix: prefix, postfix: postfix, withKohonen: withKohonen);
+            RunMap("Map3", prefix: prefix, postfix: postfix, withKohonen: withKohonen);
+            RunMap("Map4", prefix: prefix, postfix: postfix, withKohonen: withKohonen);
+            RunMap("Map5", prefix: prefix, postfix: postfix, withKohonen: withKohonen);
+
         }
 
-        private void RunMap(string map, int numOfThreads = 4, string prefix = "")
+        private void RunMap(string map, int numOfThreads = 4, string prefix = "", string postfix = "", bool withKohonen = true, int iterationsPerThread = 5000)
         {
+            // initial settings
             s.ResetToDefault();
             s.maps.Clear();
             for (int i = 0; i < numOfThreads; i++)
             {
                 s.maps.Add(map);
             }
-            s.maps.Add("Map5");
-            s.numberOfIterationsPerMap = 100;
-            s.numberOfIterationsPerMapWithKohonen = 100;
 
-            s.kohonenSaveFile = prefix + map + "_koh.txt";
-            s.qLearningSaveFile = prefix + map + "_q.txt";
+            s.kohonenSaveFile = prefix + map + "_koh_" + postfix + ".txt";
+            s.qLearningSaveFile = prefix + map + "_q_" + postfix + ".txt";
             s.kohonenLoadFile = null;
             s.qLearningLoadFile = null;
 
-            CustomLogger.filename = prefix + map + "_stats.txt";
+            CustomLogger.filename = prefix + map + "_stats_" + postfix + ".txt";
             CustomLogger.ClearActualLogFile();
 
-            s.kohonenRadius = 3;
+            // first run
+            CustomLogger.Log("############### Run 1/5: " + prefix + map + "_" + postfix + " ###############");
+            s.numberOfIterationsPerMap = iterationsPerThread;
+            s.numberOfIterationsPerMapWithKohonen = s.numberOfIterationsPerMap;
             manager.ExperimentRun(s);
-            CustomLogger.LogToFile(s.ToCSVString() + ", " + MtStats.ToCVSString(), cvsFilename);
+            CustomLogger.LogToFile(s.ToCSVString() + ", " + MtStats.ToCVSString(), csvFilename);
 
             s.kohonenLoadFile = s.kohonenSaveFile;
             s.qLearningLoadFile = s.qLearningSaveFile;
 
-            s.kohonenLearningRate = 0.3;
-            s.kohonenRadius = 2;
+            // second run
+            CustomLogger.Log("############### Run 2/5: " + prefix + map + "_" + postfix + " ###############");
+            s.kohonenLearningRate = 0.25;
             manager.ExperimentRun(s);
-            CustomLogger.LogToFile(s.ToCSVString() + ", " + MtStats.ToCVSString(), cvsFilename);
+            CustomLogger.LogToFile(s.ToCSVString() + ", " + MtStats.ToCVSString(), csvFilename);
+
+            // third run
+            CustomLogger.Log("############### Run 3/5: " + prefix + map + "_" + postfix + " ###############");
+            if (!withKohonen) s.numberOfIterationsPerMapWithKohonen = 0;
+            s.qLearningLearningRate = 0.3;
+            s.qLearningRandomActionProbability = 0.2;
+            manager.ExperimentRun(s);
+            CustomLogger.LogToFile(s.ToCSVString() + ", " + MtStats.ToCVSString(), csvFilename);
+
+            // fourth run
+            CustomLogger.Log("############### Run 4/5: " + prefix + map + "_" + postfix + " ###############");
+            s.qLearningLearningRate = 0.25;
+            s.qLearningRandomActionProbability = 0.1;
+            manager.ExperimentRun(s);
+            CustomLogger.LogToFile(s.ToCSVString() + ", " + MtStats.ToCVSString(), csvFilename);
+
+            // fifth run - final with fixed kohonen and qlearning - runs only half iterations
+            CustomLogger.Log("############### Run 5/5: " + prefix + map + "_" + postfix + " ###############");
+            s.numberOfIterationsPerMapWithKohonen = 0;
+            s.numberOfIterationsPerMap = s.numberOfIterationsPerMap/2;
+            s.qLearningRandomActionProbability = 0.0001;
+            s.qLearningLearningRate = 0.1;
+            manager.ExperimentRun(s);
+            CustomLogger.LogToFile(s.ToCSVString() + ", " + MtStats.ToCVSString(), csvFilename);
+
+            CustomLogger.LogToFile("", csvFilename);
         }
     }
 }
